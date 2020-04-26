84c6b02ff22daa1091eaa5f3f18163260e72e7f7
Jordan <jordanrcarlson@gmail.com>
2020-04-26 09:04:12 -0700

    death

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/move', (request, response) => {
 
   // Response data
   const data = {
-    move: request.body.turn == 0 ? 'up':'left' // one of: ['up','down','left','right']
+    move: request.body.turn == 1 ? 'up':'left' // one of: ['up','down','left','right']
   }
 
 
