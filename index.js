f0ee4ba3b1d7f1a3321a65ae3cfec2c0a426dcee
Jordan <jordanrcarlson@gmail.com>
2020-04-26 12:40:15 -0700

    death

--- index.js
+++ index.js
@@ -42,9 +42,27 @@ app.post('/move', (request, response) => {
   // Response data
   const data = {
 
-    move: 'right'  // one of: ['up','down','left','right']
-  }
 
+  count = 0
+	if (count % 4 = 1){
+	    move: 'up'  // one of: ['up','down','left','right']
+		count ++
+
+	} else if (count % 4 = 2){
+	    move: 'right'  // one of: ['up','down','left','right']
+		count ++
+
+	} else if (count % 4 = 3){
+	    move: 'down'  // one of: ['up','down','left','right']
+		count ++
+
+	} else if (count % 4 = 4){
+	    move: 'left '  // one of: ['up','down','left','right']
+		count ++
+	}else{
+	    move: 'up'  // one of: ['up','down','left','right']
+		count ++
+	}
 
 
 
